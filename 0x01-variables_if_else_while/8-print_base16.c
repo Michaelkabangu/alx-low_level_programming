36a6310599482972e1dbf9b7f21761465bfe42d6
Michael <michaelkabangu525@gmail.com>
2023-06-20 05:51:17 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: 'printing all numbers of base hexadecimal in lowercase)
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	int low;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	for (low = 'a'; low <= 'f'; low++)
+	{
+		putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
